4e7ec2b0af503e8ab3423c8c2e2816c47bfd4f12
Eduardo Estrada <me@e2rd0.com>
2023-10-27 16:50:16 +0200

    fix / typo in conditional

--- src/api/middlewares/session-middleware.ts
+++ src/api/middlewares/session-middleware.ts
@@ -18,7 +18,7 @@ export default class SessionMiddleware {
             if (!req.headers.authorization) throw new Error("Unauthorized");
             const sessionToken = req.headers.authorization.split(" ")[1];
             const session = JWT.VerifyToken(sessionToken, constants.JWT_SECRET) as UserSessionPayload;
-            if(session.exp < Date.now()) throw new Error("Session expired");
+            if(session.exp > Date.now()) throw new Error("Session expired");
             next();
         } catch (error) {
             next(error);
