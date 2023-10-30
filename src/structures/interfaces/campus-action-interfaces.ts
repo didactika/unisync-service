89fc0c8717641a4f02b5318eb71cacd3a76fa8ec
hector.arrechea <hector.arrechea@ct.uneatlantico.es>
2023-10-30 04:44:19 +0100

    feature: create logic to connect with moodle from webServices

---
+++ src/structures/interfaces/campus-action-interfaces.ts
@@ -0,0 +1,9 @@
+/**
+ * Campus Action interface
+ * @interface ICampusAction
+ * @description This interface is used to define the campus action required data
+ */
+export default interface ICampusAction {
+    url: string,
+    token: string
+}
