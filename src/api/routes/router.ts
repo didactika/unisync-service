944ef0a3449e054abc86f114e0f74d4d3a4036db
hector.arrechea <hector.arrechea@ct.uneatlantico.es>
2023-11-05 23:36:02 +0100

    Fix: course shortname validation and add mod routes.

--- src/api/routes/router.ts
+++ src/api/routes/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./user-routes";
 import campusRoutes from "./campus-routes";
 import courseRoutes from "./course-routes";
+import modRoutes from "./mod-routes";
 
 const router = express.Router({
     strict: true,
@@ -10,5 +11,6 @@ const router = express.Router({
 router.use("/user", userRoutes);
 router.use("/campus", campusRoutes);
 router.use("/campus/:campusUuid/course", courseRoutes);
+router.use("/campus/:campusUuid/course/:shortname/mod", modRoutes);
 
 export default router;
