7ce402b32fbe4202db8fdc68f2d9684b01a1a54c
hlaelissalt <hector.arrechea@ct.uneatlantico.es>
2025-03-21 09:19:37 +0100

    fix: modify course-campus controller

--- src/core/api/decorators/controller.ts
+++ src/core/api/decorators/controller.ts
@@ -1,29 +1,31 @@
-import { Router } from "express";
-import { RouteOrMiddlewareConfig } from "../../types/api/decorators/controller";
+import {Router} from "express";
+import {RouteOrMiddlewareConfig} from "../../types/api/decorators/controller";
 
 export function Controller(basePath: string): ClassDecorator {
-  return function (target: any) {
-    !target.prototype.basePath ? (target.prototype.basePath = basePath) : (target.prototype.basePath += basePath);
-    Object.defineProperty(target.prototype, "router", {
-      value: Router(),
-      writable: false,
-    });
+    return function (target: any) {
+        !target.prototype.basePath ? (target.prototype.basePath = basePath) : (target.prototype.basePath += basePath);
+        Object.defineProperty(target.prototype, "router", {
+            value: Router({
+                mergeParams: true
+            }),
+            writable: false,
+        });
 
-    if (target.configs) {
-      target.configs.forEach((config: RouteOrMiddlewareConfig) => {
-        if (config.type === "middleware") {
-          const method = config.method?.toLowerCase();
-          if (method && target.prototype.router[method]) {
-            target.prototype.router[method](config.path || "*", config.handler.bind(target));
-          } else if (config.path) {
-            target.prototype.router.use(config.path, config.handler.bind(target));
-          } else {
-            target.prototype.router.use(config.handler.bind(target));
-          }
-        } else if (config.type === "route") {
-          target.prototype.router[config.method](config.path, config.handler.bind(target));
+        if (target.configs) {
+            target.configs.forEach((config: RouteOrMiddlewareConfig) => {
+                if (config.type === "middleware") {
+                    const method = config.method?.toLowerCase();
+                    if (method && target.prototype.router[method]) {
+                        target.prototype.router[method](config.path || "*", config.handler.bind(target));
+                    } else if (config.path) {
+                        target.prototype.router.use(config.path, config.handler.bind(target));
+                    } else {
+                        target.prototype.router.use(config.handler.bind(target));
+                    }
+                } else if (config.type === "route") {
+                    target.prototype.router[config.method](config.path, config.handler.bind(target));
+                }
+            });
         }
-      });
-    }
-  };
+    };
 }
